15bdf1730c8c1f3a6d0a8af3a959056ac7c8aec1
MKING301 <33675411+MKing301@users.noreply.github.com>
2019-01-29 22:06:53 -0500

    Added config file with secret key using environment variable Signed-off-by: MKING301

---
+++ config.py
@@ -0,0 +1,7 @@
+"""This is the configuration file for the application."""
+import os
+
+# To generate a secret key
+# import secrets
+# secrets.token_hex(16)
+SECRET_KEY = os.environ.get('SECRET_KEY_LOCAL')
