6c7f7479b31b5f802452759725c9fb07b15ade53
MKING301 <33675411+MKing301@users.noreply.github.com>
2019-02-03 15:44:17 -0500

    Removed routes from main app, added imports and app config Signed-off-by: MKING301

--- group_blog.py
+++ group_blog.py
@@ -1,14 +1,13 @@
-from flask import Flask, render_template, url_for
+import os
+from flask import Flask
+
+# Create app
 app = Flask(__name__)
 
-@app.route("/")
-@app.route("/home")
-def home():
-    return render_template('home.html')
+# Load configuration
+app.config.from_pyfile('config.py')
 
-@app.route("/about")
-def about():
-    return render_template('about.html', title='About')
+from views import *
 
 if __name__ == '__main__':
     app.run(debug=True)
