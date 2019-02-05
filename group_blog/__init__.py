f1426301f7bc7dd04a556c41b345e64e04d1f44e
MKING301 <33675411+MKing301@users.noreply.github.com>
2019-02-05 17:24:53 -0500

    Added flask login to incorporate user management functionality Signed-off-by: MKING301

--- group_blog/__init__.py
+++ group_blog/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 # Create app
 app = Flask(__name__)
@@ -13,5 +14,8 @@ db = SQLAlchemy(app)
 
 # Set up encrpytion
 bcrypt = Bcrypt(app)
+
+# Create login manager
+login_manager = LoginManager(app)
 
 from group_blog import views
--- group_blog/views.py
+++ group_blog/views.py
@@ -8,6 +8,7 @@ from flask import render_template, url_for, flash, redirect
 from group_blog import app, db, bcrypt
 from group_blog.forms import RegistrationForm, LoginForm
 from group_blog.models import User, Post
+from flask_login import login_user, current_user
 
 
 @app.route("/")
@@ -21,6 +22,8 @@ def about():
 
 @app.route("/register", methods=['GET', 'POST'])
 def register():
+    if current_user.is_authenticated:
+        redirect(url_for('home'))
     form = RegistrationForm()
     if form.validate_on_submit():
         hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
@@ -33,11 +36,14 @@ def register():
 
 @app.route("/login", methods=['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+        redirect(url_for('home'))
     form = LoginForm()
     if form.validate_on_submit():
-        if form.email.data == 'admin@smallgroup.com' and form.password.data == 'password':
-            flash('You have been logged in sucessfully!', 'success')
+        user = User.query.filter_by(email=form.email.data).first()
+        if user and bcrypt.check_password_hash(user.password, form.password.data):
+            login_user(user, remember=form.remember.data)
             return redirect(url_for('home'))
         else:
-            flash('Login Unsuccessful. Please check credentials', 'danger')
+            flash('Login Unsuccessful. Please check email and password', 'danger')
     return render_template('login.html', title='Login', form=form)
