0b1b4ff2608c794ddceef24498055ee81ef41a24
MKING301 <33675411+MKing301@users.noreply.github.com>
2019-05-12 18:10:03 -0400

    Add email configuration

--- group_blog/__init__.py
+++ group_blog/__init__.py
@@ -1,10 +1,26 @@
+import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+from flask_mail import Mail
+
 
 # Create app
 app = Flask(__name__)
+
+# Configure Mail
+mail_settings = {
+    "MAIL_SERVER": 'smtp.gmail.com',
+    "MAIL_PORT": 587,
+    "MAIL_USE_TLS": True,
+    "MAIL_USE_SSL": False,
+    "MAIL_USERNAME": os.environ.get('MAIL_USERNAME'),
+    "MAIL_PASSWORD": os.environ.get('MAIL_PASSWORD')
+}
+
+app.config.update(mail_settings)
+mail = Mail(app)
 
 # Load configuration
 app.config.from_pyfile('config.py')
