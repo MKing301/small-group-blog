ed7d231ac9343dea16bc57e511aa875cb6e571e9
MKING301 <33675411+MKing301@users.noreply.github.com>
2019-05-27 21:14:08 -0400

    Add errors blueprint to app

--- group_blog/__init__.py
+++ group_blog/__init__.py
@@ -32,8 +32,10 @@ def create_app(config_class=Config):
     from group_blog.users.routes import users
     from group_blog.posts.routes import posts
     from group_blog.main.routes import main
+    from group_blog.errors.handlers import errors
     app.register_blueprint(users)
     app.register_blueprint(posts)
     app.register_blueprint(main)
+    app.register_blueprint(errors)
 
     return app
