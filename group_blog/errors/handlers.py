708c72841c904fdf13844559d56c2ef2443457ed
MKING301 <33675411+MKing301@users.noreply.github.com>
2019-05-27 21:26:45 -0400

    Move error templates to correct folder

--- group_blog/errors/handlers.py
+++ group_blog/errors/handlers.py
@@ -3,7 +3,6 @@ from flask import Blueprint, render_template
 errors = Blueprint('errors', __name__)
 
 
-
 @errors.app_errorhandler(404)
 def error_404(error):
     render_template('errors/404.html'), 404
@@ -14,6 +13,6 @@ def error_403(error):
     render_template('errors/403.html'), 403
 
 
-    @errors.app_errorhandler(500)
+@errors.app_errorhandler(500)
 def error_500(error):
     render_template('errors/500.html'), 500
